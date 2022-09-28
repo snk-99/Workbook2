3bbf7696971edb2639ccd3022df8f93b26d621aa
Samuel Kahura <kahurasamuel@gmail.com>
2022-09-28 09:44:35 -0400

    math scripts

---
+++ Exercises/MathScripts/netWorth.js
@@ -0,0 +1,7 @@
+//find your net worth
+var assets = 60000000;
+var debts = 500000;
+
+var netWorth = assets - debts;
+
+console.log(`My net worth is ${netWorth}`)
--- Exercises/tips.js
+++ Exercises/tips.js
@@ -1,15 +1,15 @@
+//Exercise 1
+
 // sample inputs
 var foodCost = 79.25;
 var tax = 6.54;
 var tip = 12.00;
 
-var num1 = 15;
-var num2 = 10;
-
-
 // calculations
 var totalDue = foodCost + tax + tip;
 
+// Template literal
+// `string text ${expression} string text`
 console.log(`Food cost is ${foodCost} and tax is ${tax}`);
 
 //.toFixed adds two digits to the right of the decimal point
